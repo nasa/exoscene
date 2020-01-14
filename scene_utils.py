a1976b3ff94a95f8ff3bbef388866be98112d4b4
Neil Zimmerman <neiltzim@gmail.com>
2020-01-14 15:07:40 -0500

    fixed pkg_resources file name

--- scene_utils.py
+++ scene_utils.py
@@ -771,7 +771,8 @@ def bpgs_spectype_to_photonrate(spectype,Vmag,minlam,maxlam):
     subset = dat[dat['Type']==spectype]
     if len(subset) > 0:
         specnum = dat[dat['Type']==spectype].index[0]+1
-        fname = pkg_resources.resource_filename('crispy', 'Inputs') + '/bpgs/bpgs_' + str(specnum)+ '.fits'
+        #fname = pkg_resources.resource_filename('crispy', 'Inputs') + '/bpgs/bpgs_' + str(specnum)+ '.fits'
+        fname = pkg_resources.resource_filename('scene_utils', '/bpgs/bpgs_' + str(specnum)+ '.fits')
     
         return bpgsfile_to_photonrate(fname,Vmag,minlam,maxlam)
     else:
@@ -796,7 +797,8 @@ def bpgs_to_photonrate(specnum,Vmag,minlam,maxlam):
     val: Quantity
         Photons/second/m2 coming from the star within the band
     '''
-    fname = pkg_resources.resource_filename('crispy', 'Inputs') + '/bpgs/bpgs_' + str(specnum)+ '.fits'
+    #fname = pkg_resources.resource_filename('crispy', 'Inputs') + '/bpgs/bpgs_' + str(specnum)+ '.fits'
+    fname = pkg_resources.resource_filename('scene_utils', '/bpgs/bpgs_' + str(specnum)+ '.fits')
     return bpgsfile_to_photonrate(fname,Vmag,minlam,maxlam)
 
 def bpgsfile_to_photonrate(filename,Vmag,minlam,maxlam):
@@ -849,3 +851,40 @@ def input_star(filename,Vmag,wavel):
     flux = func(wavel)*u.W/u.m**2/u.nm
     Eph = (c.c*c.h/(wavel*u.nm)/u.photon).to(u.J/u.photon)
     return (flux/Eph).to(u.photon/u.s/u.m**2/u.nm)
+
+def calc_contrast(wavelist, distance, radius, filename, albedo=None):
+    '''
+    Function calcContrast
+
+    Returns the flux ratio Fplanet/Fstar at given wavelengths.
+
+    Parameters
+    ----------
+    wavelist :   1D array, list
+            Array of wavelengths in nm at which the contrast is computed.
+    distance : float
+            Distance in AU between the planet and the star
+    radius: float
+            Radius of planet in units of Jupiter radii
+    filename: string
+            Two-column file with first column as the wavelength in microns, second column is the geometrical albedo
+    albedo: float
+            If None, then the albedo is given by the contents of the text file. If not None, the geometrical albedo given
+            in the text file is normalized to have its maximum within the wavelist range to be albedo.
+
+    Returns
+    -------
+    vals : 1D array
+            Array of flux ratio at the desired wavelengths.
+
+    '''
+
+    spectrum = np.loadtxt(filename)
+    spec_func = scipy.interpolate.interp1d(spectrum[:, 0] * 1000., spectrum[:, 1])
+    vals = spec_func(wavelist)
+    if albedo is not None:
+        vals /= np.amax(vals)
+        vals *= albedo
+
+    vals = vals * (radius * c.R_jup.to(u.m) / (distance * u.AU).to(u.m))**2
+    return vals
