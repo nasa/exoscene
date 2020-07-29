064057fd6115a8c436dc144692b202120e2d7f98
Neil Zimmerman <neiltzim@gmail.com>
2020-07-28 22:31:53 -0400

    moved bpgs data into data subdir

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 setup(
   name='exoscene',
+  description='Library for simulating direct images of exoplanetary systems.',
   version='1.0',
   author='Neil T. Zimmerman',
   author_email='neil.t.zimmerman@nasa.gov',
+
   packages=['exoscene'],
-  description='Library for simulating direct images of exoplanetary systems.',
+  package_data={'exoscene': ['./data/bpgs/bpgs_*.fits',
+                             './data/bpgs/bpgs_readme.csv']},
+
   install_requires=[
       "astropy >= 4.0",
       "numpy",
